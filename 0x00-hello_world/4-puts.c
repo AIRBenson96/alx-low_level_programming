9bde2bb086cac72f63d3c95cdfe4fd93ce136f5f
Asueze Igwe <igweasueze6@gmail.com>
2023-06-15 13:26:08 -0700

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * Description: main will print the string to stdout
  * Return 0 if successful
- *
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-i}
+}
