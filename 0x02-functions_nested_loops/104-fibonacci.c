f650f90d1f44b2597cc439dc213725c8e3b43a66
Asueze Igwe <igweasueze6@gmail.com>
2023-06-28 03:44:20 -0700

    Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+unsigned long int fib1 = 1, fib2 = 2, nextFib;
+int count;
+
+printf("%lu, %lu", fib1, fib2);
+
+for (count = 3; count <= 98; count++)
+{
+nextFib = fib1 + fib2;
+printf(", %lu", nextFib);
+
+fib1 = fib2;
+fib2 = nextFib;
+}
+
+printf("\n");
+
+return (0);
+}
