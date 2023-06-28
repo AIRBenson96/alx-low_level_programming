7b6071359e3c2bf3a9b0f8f3bc247ee15737089e
Asueze Igwe <igweasueze6@gmail.com>
2023-06-28 02:53:00 -0700

    Write a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,10 +10,7 @@ void print_times_table(int n)
 int i, j;
 
 if (n < 0 || n > 15)
-{
-printf("Invalid input. The value of n must be between 0 and 15.\n");
 return;
-}
 
 for (i = 0; i <= n; i++)
 {
